e82d10e57fa67216f0cc93dace1deb439b3ea59e
ZmeykaTatiana <zacharovanaja@mail.ru>
2024-06-03 19:12:10 +0300

    description

--- src/test/java/org/example/SelenideGoogle.java
+++ src/test/java/org/example/SelenideGoogle.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.Test;
 
 
@@ -9,6 +10,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class SelenideGoogle {
 
     @Test
+    @Description("first test")
     public void openGoogle(){
         open("https://google.com");
         $x("//textarea[@class='gLFyf']").setValue("Minsk").pressEnter();
